3f8960a3eba2ec6e1cc49ae1787b503437d7f92e
zhouyu <zhouyu@auguigu.com>
2020-09-14 11:50:58 +0800

    master second commit

--- git_01/src/test/java/test1.java
+++ git_01/src/test/java/test1.java
@@ -2,6 +2,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello....master");
+        System.out.println("hotfix to master");
         System.out.println("hello....hotfix");
         System.out.println("hello");
         System.out.println("hello");
