5daad57f31cea8c2c70428730d47427a71a29cc6
zhouyu <zhouyu@auguigu.com>
2020-09-14 14:09:17 +0800

    master third commit

--- git_01/src/test/java/test1.java
+++ git_01/src/test/java/test1.java
@@ -5,7 +5,7 @@ public class test1 {
         System.out.println("hotfix to master");
         System.out.println("hello....hotfix");
         System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello.....hotfix2");
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
